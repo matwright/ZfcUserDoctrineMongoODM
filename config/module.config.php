f070f088ab1d83417df63dd2387f8129ae639aea
Mat Wright <mat.wright@broadshout.com>
2020-06-19 19:35:21 +0200

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,17 @@
 <?php
 return array(
-  
+    'doctrine' => array(
+        'driver' => array(
+            'zfcuser_document' => array(
+                'class' => 'Doctrine\ODM\MongoDB\Mapping\Driver\XmlDriver',
+                'paths' => [__DIR__ . '/xml']
+            ),
+
+            'odm_default' => array(
+                'drivers' => array(
+                    'ZfcUserDoctrineMongoODM\Document'  => 'zfcuser_document'
+                )
+            )
+        )
+    ),
 );
